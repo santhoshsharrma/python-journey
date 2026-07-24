49be0eedb8f1e4ee91329dea6e5009aec3c700cc
Santhosh Sharma <santhoshsharmapaturi@gmail.com>
2026-07-24 23:33:54 +0530

    Added case insensitive input and comments explanation

--- quiz.py
+++ quiz.py
@@ -1,16 +1,17 @@
+# This is a mini quiz game that i created in order to understand few concepts.
 name = input("Enter your name: ")
 print("Welcome to my quiz " + name + "!")
-desire_play = input("Do you want to play the quiz game, " + name + "? ")
+desire_play = input("Do you want to play the quiz game, " + name + "? ").lower() # .lower() makes our response case insensitive ex: YES,yEs,yes 
 if(desire_play == "yes"):
     print("Let's do it! ")
 else:
     print("Goodbye! ")
     quit()
-score = 0
+score = 0 # This is to check our score points and begins from 0
 question = input("What does AI stand for? ")
 if question.lower() == "artificial intelligence":
     print('Correct!')
-    score += 1
+    score += 1 # For every Response, if it's TRUE, adds a point else adds nothing
 else:
     print('Oops, wrong answer!')
     
@@ -28,4 +29,4 @@ else:
     print('Absolutely right ' + name+ '!' + " Even if AI becomes superintelligence, humans still has to reverify the decision")
     score += 1
     
-print("Congrats " + name + "! " + "You earned " + str(score) + "%" + " points")
+print("Congrats " + name + "! " + "You earned " + str(score) + "%" + " points") # This is to check our final points gained
